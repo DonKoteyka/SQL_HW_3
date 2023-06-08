befb26161b899f0d85b93ec0483e938690945903
Don Koteyka <777zik@gmail.com>
2023-06-08 20:56:22 +0300

    upadte 2-3

--- task_2-4.sql
+++ task_2-4.sql
@@ -12,32 +12,32 @@ select name from soundtrack where name like '%мой%' or name like '%My%' or na
 
 --Задание 3
 --    Количество исполнителей в каждом жанре.
-select type, count(nickname) from genre g 
-join singergenre s on g.id = s.genreid 
-join singer s2 on s.singerid = s2.id 
+select type, count(nickname) from genre as g 
+join singergenre as s on g.id = s.genreid 
+join singer as s2 on s.singerid = s2.id 
 group by g.type
 --    Количество треков, вошедших в альбомы 2019–2020 годов.
-select coll.year, count(s4.name) from collection coll 
-join soundtrackcollection s3  on coll.id = s3.collectionid  
-join soundtrack s4 on s3.soundtrackid  = s4.id 
+select coll.year, count(s4.name) from collection as coll 
+join soundtrackcollection as s3  on coll.id = s3.collectionid  
+join soundtrack as s4 on s3.soundtrackid  = s4.id 
 where coll.year between 2019 and 2020
 group by coll.year
 --    Средняя продолжительность треков по каждому альбому.
 select a.name, avg(s5.time) from album a
-join soundtrack s5 on s5.album_id = a.id
+join soundtrack as s5 on s5.album_id = a.id
 group by a.name
 --    Все исполнители, которые не выпустили альбомы в 2020 году.
 select distinct nickname from singer si1
-join singeralbum s6 on s6.singerid = si1.id
-join album a on s6.albumid = a.id
+join singeralbum as s6 on s6.singerid = si1.id
+join album as a on s6.albumid = a.id
 where a.year !=2020 
 --    Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
 select distinct coll.name, nickname from singer si1
-join singeralbum s6 on s6.singerid = si1.id
-join album a on s6.albumid = a.id
-join soundtrack s5 on s5.album_id = a.id
-join soundtrackcollection s3 on s3.soundtrackid = s5.id
-join collection coll  on coll.id = s3.collectionid  
+join singeralbum as s6 on s6.singerid = si1.id
+join album as a on s6.albumid = a.id
+join soundtrack as s5 on s5.album_id = a.id
+join soundtrackcollection as s3 on s3.soundtrackid = s5.id
+join collection as coll  on coll.id = s3.collectionid  
 where nickname = 'Rob Zombie'
 
 
